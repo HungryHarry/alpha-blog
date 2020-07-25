6d9a093633b26dd7ac76593eb86d16e042b06dfb
HungryHarry <kai.nass90@gmail.com>
2020-07-25 14:53:02 +0200

    Navigation added, also delete,edit,show links and methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,21 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      # redirect_to article_path(@article) can be shortened
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -22,5 +37,11 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
