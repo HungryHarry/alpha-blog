b248db01b85503cc3bf289a0f7f6ab92b7767573
HungryHarry <kai.nass90@gmail.com>
2020-08-12 14:48:10 +0200

    User validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-
+  validates :username, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        length: {minimum: 3, maximum: 85}
+  VALIDATES_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, uniqueness: true,
+                        presence: true,
+                        length: { maximum: 105 },
+                        format: { with: VALIDATES_EMAIL_REGEX }
 end
